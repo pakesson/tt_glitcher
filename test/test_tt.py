1fae0e1a64f3ec717cd8ff28abbd5510ae45c67c
Philip Åkesson <philip.akesson@gmail.com>
2026-02-14 18:12:36 +0100

    Add top level trigger test

--- test/test_tt.py
+++ test/test_tt.py
@@ -89,3 +89,58 @@ async def test_project_full_glitch_sequence(dut):
 
     await ClockCycles(dut.clk, 1)
     assert dut.pulse_out.value == 0, "Expected pulse_out to be 0"
+
+@cocotb.test()
+async def test_project_trigger(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 20 ns (50 MHz)
+    clock = Clock(dut.clk, 20, unit="ns")
+    cocotb.start_soon(clock.start())
+
+    # Initial values
+    dut.trigger_in.value = 0
+
+    # Reset
+    dut._log.info("Reset")
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut._log.info("Test project full glitch sequence")
+
+    uart_source = UartSource(dut.uart_rx, baud=115200, bits=8, stop_bits=1)
+
+    await uart_source.write(b'\x00\x00\x12') # Set delay
+    await uart_source.write(b'\x01\x01')     # Set width
+    await uart_source.write(b'\x02\x02')     # Set num pulses
+    await uart_source.write(b'\x03\x00\x03') # Set pulse spacing
+    await uart_source.wait()
+
+    await ClockCycles(dut.clk, 1)
+
+    dut.trigger_in.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.trigger_in.value = 0
+
+    for _ in range(0x13): # Delay + 1
+        await ClockCycles(dut.clk, 1)
+        assert dut.pulse_out.value == 0, "Expected pulse_out to be 0"
+
+    for _ in range(2): # Width + 1
+        await ClockCycles(dut.clk, 1)
+        assert dut.pulse_out.value == 1, "Expected pulse_out to be 1"
+
+    for _ in range(4): # Spacing + 1
+        await ClockCycles(dut.clk, 1)
+        assert dut.pulse_out.value == 0, "Expected pulse_out to be 0"
+
+    for _ in range(2): # Width + 1
+        await ClockCycles(dut.clk, 1)
+        assert dut.pulse_out.value == 1, "Expected pulse_out to be 1"
+
+    await ClockCycles(dut.clk, 1)
+    assert dut.pulse_out.value == 0, "Expected pulse_out to be 0"
